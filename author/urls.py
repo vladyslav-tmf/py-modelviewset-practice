186a1f82b02f01c2c5aea886063db6bc3b3d7c0e
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-23 22:26:18 +0200

    Add author API with viewset and URL routing
    - Implemented a viewset for the Author model and connected it to API routes.

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+from author import views
+
+
 app_name = "author"
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register("authors", views.AuthorViewSet, basename="manage")
+
+urlpatterns = [path("", include(router.urls))]
